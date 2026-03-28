94737792341fbdf8c78c39b25e375c1af532db91
Nandini Gulhane <nandinigulhane8@gmail.com>
2026-03-28 23:46:31 +0530

    chore: initial commit

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.11.0/firebase-app.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDtT1vgFgZfl-CgDQ-kRsffnMjKAsLv5u8",
+  authDomain: "streakify-ad5f7.firebaseapp.com",
+  projectId: "streakify-ad5f7",
+  storageBucket: "streakify-ad5f7.firebasestorage.app",
+  messagingSenderId: "480610650087",
+  appId: "1:480610650087:web:5596b2a8c2b7f043bd5f42",
+};
+
+const app = initializeApp(firebaseConfig);
